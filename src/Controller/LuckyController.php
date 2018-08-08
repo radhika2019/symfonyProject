05afba77b9fd29461e814cef183a3289b6fe0c35
Radhika Choudhary <radhika@mialinator.com>
2018-08-08 22:15:27 +0530

    08-08-2018

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -26,6 +26,36 @@ class LuckyController extends AbstractController
         return new Response('Welcome to first page');
 	}
 
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function article($article_name= 'hello')
+    {
 
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!'.$article_name,
+            'Woohoo! I m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig',[
+            'title' => ucwords(str_replace('-', ' ', $article_name)),
+            'comments' => $comments,
+        ]);
+
+    }
+    /**
+     * @Route("/practice/extend")
+     */
+    public function twig(){
+        return $this->render('article/extend.html.twig');
+    }
+
+    /**
+     * @Route("/practice/example")
+     */
+    public function twigme(){
+        return $this->render('article/example.html.twig');
+    }
 }
 ?>
